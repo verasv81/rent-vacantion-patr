078899f40c37abb2a9b4864a88a2ab1d676c8b76
Vera Șveț <verasv997@gmail.com>
2020-02-17 11:07:30 +0200

    Initial Commit

---
+++ RentVacation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RentVacation
+//
+//  Created by Vera Svet on 2/17/20.
+//  Copyright © 2020 Vera Svet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
